fb3cda6feb0d54a3f5278f96959a180bd7c459ea
arnu515 <arnu515@protonmail.com>
2021-07-11 15:59:01 +0530

    Add `run()` function and use the Layout template

--- trash_dash/__init__.py
+++ trash_dash/__init__.py
@@ -1,11 +1,26 @@
-from rich.layout import Layout
+from rich.align import Align
+from rich.columns import Columns
 from rich.live import Live
 
-layout = Layout()
+from trash_dash.screen import Screen
 
-try:
-    with Live(layout, screen=True) as live:
-        while True:
-            pass
-except KeyboardInterrupt:
-    pass
+screen = Screen(
+    "main",
+    header_renderable=Columns(
+        [
+            Align("[bold u]TrashDash[/]", align="left", vertical="middle"),
+            Align("[bold]Settings[/]", align="right", vertical="middle"),
+        ],
+        expand=True,
+    ),
+)
+
+
+def run():
+    """Run the app"""
+    try:
+        with Live(screen.layout, screen=True):
+            while True:
+                pass
+    except KeyboardInterrupt:
+        pass
