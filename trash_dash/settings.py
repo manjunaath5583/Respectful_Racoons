634776c4a885bdfe201c86717b0817d10ce7d444
arnu515 <arnu515@protonmail.com>
2021-07-12 20:59:23 +0530

    Add `settings.py`

---
+++ trash_dash/settings.py
@@ -0,0 +1,71 @@
+"""Contains methods for manipulating settings.toml"""
+import os
+from typing import Any
+
+import toml
+
+PATH = os.path.abspath(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/settings.toml")
+)
+
+
+def _get_settings() -> dict:
+    """Gets settings from settings.toml, or creates it if it doesn't exist, or is invalid"""
+    if not os.path.exists(PATH):
+        with open(PATH, "w") as f:
+            f.write("# Settings")
+    # Try reading TOML and see if it is valid
+    try:
+        with open(PATH, "r") as f:
+            return toml.load(f)
+    except toml.TomlDecodeError:
+        with open(PATH, "w") as f:
+            f.write("# Settings")
+    return {}
+
+
+def _write_settings(data: dict) -> dict:
+    with open(PATH, "w") as f:
+        toml.dump(data, f)
+    return data
+
+
+class _Settings:
+    __name: str
+
+    def __init__(self, name: str):
+        """
+        An interface for accessing settings of a module
+
+        :param name: Name of the module
+        """
+        self.__name = name
+
+    @property
+    def name(self) -> str:
+        """Name of the module"""
+        return self.__name
+
+    def get(self, key: str) -> Any:
+        """Gets a setting"""
+        settings = _get_settings()
+        if type(settings.get(self.name)) != dict:
+            raise Exception("Setting not registered")
+        return settings[self.name].get(key)
+
+    def set(self, key: str, value: Any) -> Any:
+        """Gets a setting"""
+        settings = _get_settings()
+        if type(settings.get(self.name)) != dict:
+            raise Exception("Setting not registered")
+        settings[self.name][key] = value
+        _write_settings(settings)
+
+
+def register(name: str):
+    """Register a module in settings"""
+    settings = _get_settings()
+    if type(settings.get(name)) != dict:
+        settings[name] = {"name": name}
+    _write_settings(settings)
+    return _Settings(name)
