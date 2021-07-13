eb61238b0406acbfc273dfc4a87460786075b4d7
arnu515 <arnu515@protonmail.com>
2021-07-13 16:14:23 +0530

    render cards

--- trash_dash/body.py
+++ trash_dash/body.py
@@ -3,9 +3,10 @@ from collections.abc import Callable
 from typing import Tuple
 
 from rich.columns import Columns
-from rich.console import RenderableType
+from rich.console import RenderableType, RenderGroup
 from rich.padding import Padding
 
+from trash_dash import cards
 from trash_dash.date_time_section import date_time_section
 from trash_dash.modules.today import TodayModule
 
@@ -14,10 +15,29 @@ def body() -> Tuple[RenderableType, Callable]:
     """Render the body of the main page"""
     today, today_destroy = TodayModule.card()
     destroy_func = [today_destroy]
+    one, destroy_one = cards.one()
+    two, destroy_two = cards.two()
+    three, destroy_three = cards.three()
 
     def destroy():
         for i in destroy_func:
             if hasattr(i, "__call__"):
                 i()
+        destroy_one()
+        destroy_two()
+        destroy_three()
 
-    return Padding(Columns([today, date_time_section()], expand=True), (1, 3)), destroy
+    return (
+        Padding(
+            Columns(
+                [
+                    today,
+                    RenderGroup(date_time_section(), Padding(one, (2, 0))),
+                    RenderGroup(two, Padding(three, (2, 0))),
+                ],
+                expand=True,
+            ),
+            (1, 3),
+        ),
+        destroy,
+    )
