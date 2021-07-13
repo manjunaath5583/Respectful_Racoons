301ff70e90315786f01beb6094f0f6e2424d907b
arnu515 <arnu515@protonmail.com>
2021-07-13 18:35:09 +0530

    (re)render the main screen on pressing escape

--- trash_dash/__init__.py
+++ trash_dash/__init__.py
@@ -2,54 +2,31 @@ from typing import Optional
 
 from blessed import Terminal, keyboard
 from rich import print
-from rich.align import Align
-from rich.columns import Columns
-from rich.console import RenderGroup
 from rich.live import Live
-from rich.padding import Padding
 
-from trash_dash.body import body
-from trash_dash.screen import Screen
-from trash_dash.settings import get_settings
-
-app_settings = get_settings()
-body_renderable, body_destroy = body()
-screen = Screen(
-    "main",
-    header_renderable=Padding(
-        RenderGroup(
-            Columns(
-                [
-                    Align(
-                        f"[bold u]{app_settings.get('app_name', 'TrashDash')}[/]",
-                        align="left",
-                        vertical="middle",
-                    ),
-                    Align(
-                        "[bold]Settings    Exit(q)[/]", align="right", vertical="middle"
-                    ),
-                ],
-                expand=True,
-            ),
-            Align("Use the [yellow]keyboard[/] to navigate!", align="center"),
-        ),
-        (1, 3, 0, 3),
-    ),
-    body_renderable=body_renderable,
-)
+from trash_dash.main_screen import create_screen
+from trash_dash.screen import screens
 
 term = Terminal()
 
 
 def run():
     """Run the app"""
+    current_destroy = create_screen()[1]
+
     try:
         with term.fullscreen(), term.cbreak():
-            with Live(screen.layout, screen=True):
+            with Live(screens["main"].layout, screen=True) as live:
                 pressed_key: Optional[keyboard.Keystroke] = None
                 while pressed_key != "q":
                     pressed_key = term.inkey()
-        body_destroy()
+                    if pressed_key.is_sequence and pressed_key.code == 361:
+                        # Re-render the main screen when <ESC> is pressed
+                        x, y = create_screen()
+                        live.update(x.layout)
+                        current_destroy()
+                        current_destroy = y
+        current_destroy()
         print("[b]Exiting!")
     except KeyboardInterrupt:
         pass
