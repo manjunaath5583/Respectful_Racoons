0b8ff78de6a1c5876363b46a66966b93840e3782
arnu515 <arnu515@protonmail.com>
2021-07-13 18:56:24 +0530

    show more button

--- trash_dash/__init__.py
+++ trash_dash/__init__.py
@@ -1,13 +1,39 @@
-from typing import Optional
+from collections.abc import Callable
+from typing import Optional, Tuple
 
 from blessed import Terminal, keyboard
 from rich import print
+from rich.align import Align
+from rich.console import RenderableType, RenderGroup
 from rich.live import Live
+from rich.panel import Panel
 
+from trash_dash.cards import cards as _cards
 from trash_dash.main_screen import create_screen
+from trash_dash.modules import modules
 from trash_dash.screen import screens
 
 term = Terminal()
+
+
+def _show_more(card_index: int) -> Optional[Tuple[RenderableType, Callable]]:
+    try:
+        card_item = modules.get(_cards[card_index])
+    except IndexError:
+        return None
+    if not (hasattr(card_item, "display") and bool(card_item.display)):
+        return None
+    x = card_item.display()
+    try:
+        if not x[0]:
+            return None
+        if len(x) < 2 or not x[1] or not hasattr(x[1], "__call__"):
+            destroy = lambda: None  # noqa: E731
+        else:
+            destroy = x[1]
+        return x[0], destroy
+    except (IndexError, TypeError):
+        return None
 
 
 def run():
@@ -26,6 +52,26 @@ def run():
                         live.update(x.layout)
                         current_destroy()
                         current_destroy = y
+                    elif pressed_key in ["1", "2", "3"]:
+                        # Render the corresponding card
+                        card_index = int(pressed_key) - 1
+                        card = _show_more(card_index)
+                        if not card:
+                            live.update(
+                                Panel(
+                                    Align(
+                                        RenderGroup(
+                                            "[b]That module doesn't support being displayed on its own.",
+                                            "Press ESC to go back to the main screen",
+                                        ),
+                                        vertical="middle",
+                                    )
+                                )
+                            )
+                        else:
+                            live.update(card[0])
+                            current_destroy()
+                            current_destroy = card[1]
         current_destroy()
         print("[b]Exiting!")
     except KeyboardInterrupt:
