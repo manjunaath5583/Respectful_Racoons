56a291b07319904b7ff80ee48aa0659cc2ab94ad
arnu515 <arnu515@protonmail.com>
2021-07-15 10:21:28 +0530

    send events

--- trash_dash/main_screen.py
+++ trash_dash/main_screen.py
@@ -5,7 +5,7 @@ from rich.console import RenderGroup
 from rich.padding import Padding
 
 from trash_dash.body import body
-from trash_dash.events import once
+from trash_dash.events import emit, on, once
 from trash_dash.screen import Screen
 from trash_dash.settings import get_settings
 
@@ -13,7 +13,7 @@ from trash_dash.settings import get_settings
 def create_screen() -> Screen:
     """Creates and returns a screen"""
     app_settings = get_settings()
-    body_renderable, body_destroy = body()
+    body_renderable, body_destroy, body_update = body()
     screen = Screen(
         "main",
         header_renderable=Padding(
@@ -40,6 +40,11 @@ def create_screen() -> Screen:
         body_renderable=body_renderable,
     )
 
+    def el():
+        screen.render_body(body_update())
+        emit("main.update", screen.layout)
+
     once("main.destroy", body_destroy)
+    on("main.event_loop", el)
 
     return screen
